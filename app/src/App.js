1abf4906ff5c6bdca6a023f0a729a04bfb1b2a28
mamemaru00 <k.shouhei0711@gmail.com>
2023-08-20 23:33:45 +0900

    0表示

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,22 @@
-
-
+import { useState } from "react";
+import Button from "./components/Button";
 
 function App() {
+  const [left, setLeft] = useState(0);
+  const [ope, setOpe] = useState(null);
+  const [right, setRight] = useState(0);
+  const [ans, setAns] = useState(null);
   return (
     <div className="calc">
       <header>電卓</header>
       <div className="display">
-        {/* ここに計算結果表示 */}
+        {left}
+        {ope === null ? "" : ope}
+        {ope === null ? "" : right}
       </div>
       <div className="input">
         <div className="numbers">
-          {/* ここに数字のボタンや「.」、「=」ボタン */}
+          <Button text="0" />
         </div>
         <div className="operators">
           {/* ここに「+」、「-」、「*」、「/」ボタン */}
@@ -21,4 +27,3 @@ function App() {
 }
 
 export default App;
-
---
+++ app/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Button = (props) => { // ここをpropsに変更
+    const {
+        text,
+        ...other
+    } = props; // ここもpropsに変更
+    return (
+        <button {...other}>
+            {text}
+        </button>
+    );
+};
+
+export default Button;
